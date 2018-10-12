b44b7db120d34df8e581719f711013ec67aa6702
Cathy Li <cathysli@outlook.com>
2018-10-12 11:08:29 -0500

    Added HEaderFooterReusableView class file

---
+++ group15_assignment5/HeaderFooterCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  HeaderFooterCollectionReusableView.swift
+//  group15_assignment5
+//
+//  Created by Cathy Li on 10/12/18.
+//  Copyright © 2018 CS329E. All rights reserved.
+//
+
+import UIKit
+
+class HeaderFooterCollectionReusableView: UICollectionReusableView {
+        
+}
